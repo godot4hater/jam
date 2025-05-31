75e0ef75d9252142f07765d7047772152386adae
fishbuttgames <fishbuttgames@proton.me>
2025-05-31 17:20:18 +0100

    theyre telling me to

--- chute.gd
+++ chute.gd
@@ -5,11 +5,10 @@ extends Node3D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_area_entered(area):
+func _on_body_entered(body):
 	print("NOM NOM NOM")
-	area.get_parent().queue_free()
+	body.queue_free()
