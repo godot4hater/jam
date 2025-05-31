2bb62efb62c2393552a63813e54cc99382b11a85
fishbuttgames <fishbuttgames@proton.me>
2025-05-31 12:16:40 +0100

    random spawner object
    spawner of trash

---
+++ scenes_levels/global_spawner.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_spawner_timer_timeout():
+	print("Spawning!")
+	var children_array = get_children()
+	var childless_children = []
+	
+	for n in range(1, children_array.size()):
+		if children_array[n].get_child_count() == 1:
+			childless_children.append(children_array[n])
+			
+	if !childless_children.is_empty():
+		print("Spaning trash!")
+		var random = randi() % childless_children.size()
+		childless_children[random].instance_object()
+	else:
+		print("No space!")
+		
+	$spawnerTimer.wait_time = 5.0
+	$spawnerTimer.start()
--- scripts_test/object_spawner.gd
+++ scripts_test/object_spawner.gd
@@ -2,24 +2,14 @@ extends Node3D
 
 var scene_to_instance = preload("res://object.tscn")
 var spawning = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if get_child_count() == 1 && spawning == 0:
-		print("Object deleted...")
-		spawning = 1
-		var timer = get_child(0)
-		timer.start_timer()
-		
-
-func _on_timer_timeout():
-	print("Spawning object...")
-	spawning = 0
-	instance_object()
+	pass
 	
 func instance_object():
 	var object = scene_to_instance.instantiate()
