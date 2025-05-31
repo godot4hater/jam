6b092c246c42dc82712d91229cf080bc7aa92048
fishbuttgames <fishbuttgames@proton.me>
2025-05-31 17:05:15 +0100

    help me
    the voices

--- chute.gd
+++ chute.gd
@@ -1,15 +1,15 @@
 extends Node3D
 
-var scene_to_instance = preload("res://test_trash_pile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func instance_object():
-	var object = scene_to_instance.instantiate()
-	call_deferred("add_child",object)
+
+func _on_area_entered(area):
+	print("NOM NOM NOM")
+	area.get_parent().queue_free()
