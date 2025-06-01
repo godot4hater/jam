d8a09883a93ac3edfa2a7a7c3878779d611aff2d
godot4hater <kramericadota@gmail.com>
2025-06-01 00:02:32 -0400

    fixed endless mode not lowering lava
    quick fix

--- scripts_prod/chute.gd
+++ scripts_prod/chute.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var gameLoopRef = get_tree().get_root().get_node ("testlvl")
+@onready var lavaRef : AnimatableBody3D = get_tree().get_root().get_node ("testlvl/Lava")
 var gameLoopDisabled = false
 signal reduceTrashCounter()
 signal playerWins()
@@ -26,7 +27,17 @@ func CheckIfPlayerWon():
 	
 	if not gameLoopDisabled:
 		reduceTrashCounter.emit()
-	
+		
+		if gMode.endless:
+			gameLoopRef.allowRaise = false
+			lavaRef.global_position.y -= 2.5
+			var timer2: Timer = Timer.new()
+			timer2.wait_time = 2
+			timer2.one_shot = false
+			timer2.autostart = true
+			timer2.timeout.connect (_on_timer_timeout2)
+			add_child (timer2)
+		
 	if gameLoopRef.trashCollected >= 10 and not gameLoopDisabled and not gMode.endless:
 		playerWins.emit()
 		
@@ -41,3 +52,6 @@ func CheckIfPlayerWon():
 	
 func _on_timer_timeout() -> void:
 	get_tree().call_deferred("change_scene_to_file", "res://scenes_levels/main_menu.tscn")
+	
+func _on_timer_timeout2() -> void:
+	gameLoopRef.allowRaise = true
